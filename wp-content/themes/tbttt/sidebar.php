4be25875664ebaa130adbe80d65a2421c85ac167
brianotoole <botoole518@gmail.com>
2016-07-30 00:53:59 -0400

    rm expolore past events

--- wp-content/themes/tbttt/sidebar.php
+++ wp-content/themes/tbttt/sidebar.php
@@ -21,24 +21,4 @@
 		  <span unselectable="on"></span><span>Share on Twitter</span>
 		  </a>
 	  </div><!--/.social-share-->
-	  
-	  
-<aside class="widget widget_categories"> 
-  <h1 class="widget-title">Explore Past Events</h1>
-  
-  <ul>
-<?php $custom_query = new WP_Query('cat=7'); //past-events cat id#
-while($custom_query->have_posts()) : $custom_query->the_post(); ?>
-
-   <li class="cat-item">
-     <a href="<?php the_permalink(); ?>" rel="bookmark" title="Click to Read">
-	     <?php the_title(); ?>
-     </a>
-   </li>
-
-<?php endwhile; ?>
-<?php wp_reset_postdata(); // reset the query ?>
-  </ul>
-</aside>
-
 	</div><!-- #secondary --> 
